c77d9ed62f477622f1d3f7003795dcf55db8c25c
Tiffani Rice <tiffanirice414@gmail.com>
2023-04-17 17:53:14 -0700

    Random Images

--- js/app.js
+++ js/app.js
@@ -14,33 +14,68 @@ let image3 = document.querySelector('#imgContainer img:nth-child(3)');
 function Odd(name, fileExtension = 'jpg') {
   this.name = name;
   this.src = `img/${name}.${fileExtension}`;
+  this.views = 0;
+  this.votes = 0;
 }
 
-let bag = new Odd ('bag');
-let banana = new Odd ('banana');
-let bathroom = new Odd ('bathroom');
-let boots = new Odd ('boots');
-let breakfast = new Odd ('breakfast');
-let bubblegum = new Odd ('bubblegum');
-let chair = new Odd ('chair');
-let cthulhu = new Odd ('cthulhu');
-let dogDuck = new Odd ('dogDuck');
-let dragon = new Odd ('dragon');
-let pen = new Odd ('pen');
-let petSweep = new Odd ('petSweep');
-let scissors = new Odd ('scissors');
-let shark = new Odd ('shark');
-let sweep = new Odd ('sweep');
-let tauntaun = new Odd ('tauntaun');
-let unicorn = new Odd ('unicorn');
-let waterCan = new Odd ('waterCan');
-let wineGlass = new Odd ('wineGlass');
+let bag = new Odd('bag');
+let banana = new Odd('banana');
+let bathroom = new Odd('bathroom');
+let boots = new Odd('boots');
+let breakfast = new Odd('breakfast');
+let bubblegum = new Odd('bubblegum');
+let chair = new Odd('chair');
+let cthulhu = new Odd('cthulhu');
+let dogDuck = new Odd('dogDuck');
+let dragon = new Odd('dragon');
+let pen = new Odd('pen');
+let petSweep = new Odd('petSweep');
+let scissors = new Odd('scissors');
+let shark = new Odd('shark');
+let sweep = new Odd('sweep', 'png');
+let tauntaun = new Odd('tauntaun');
+let unicorn = new Odd('unicorn');
+let waterCan = new Odd('waterCan');
+let wineGlass = new Odd('wineGlass');
 
 oddArray.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, waterCan, wineGlass);
 
 console.log(oddArray);
 
-image1.src = oddArray[4].src;
-console.log (image2);
-image2.src = oddArray[3].src;
-image3.src = oddArray[2].src;
+function selectRandomOddNumber() {
+  return Math.floor(Math.random() * oddArray.length);
+}
+function renderOdd() {
+  let odd1 = selectRandomOddNumber();
+  let odd2 = selectRandomOddNumber();
+  if (odd2 === odd1) {
+    odd2 = selectRandomOddNumber();
+  }
+  let odd3 = selectRandomOddNumber();
+  if (odd3 === odd2 || odd3 === odd1) {
+    odd3 = selectRandomOddNumber();
+  }
+
+  image1.src = oddArray[odd1].src;
+  image1.alt = oddArray[odd1].name;
+  oddArray[odd1].views++;
+  image2.src = oddArray[odd2].src;
+  image2.alt = oddArray[odd2].name;
+  oddArray[odd2].views++;
+  image3.src = oddArray[odd3].src;
+  image3.alt = oddArray[odd3].name;
+  oddArray[odd3].views++;
+}
+
+function handleOddClick(event) {
+  console.log(event.target.alt);
+  let clickedOdd = event.target.alt;
+  for (let i = 0; i < oddArray.length; i++) {
+    if (clickedOdd === oddArray[i].name) {
+      oddArray[i].votes++;
+    }
+  }
+}
+renderOdd();
+
+myContainer.addEventListener('click', handleOddClick);
