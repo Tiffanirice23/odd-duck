31f033b4356f59d266bce50aaba6eff69ea29e12
Tiffani Rice <tiffanirice414@gmail.com>
2023-04-18 14:36:25 -0700

    Working graph

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 let oddArray = [];
 
+
 let myContainer = document.querySelector('#imgContainer');
 
 let image1 = document.querySelector('#imgContainer img:first-child');
@@ -14,6 +15,7 @@ let viewResultsBtn = document.querySelector('#resultsDiv button');
 
 let counter = 0;
 let maxCounter = 25;
+let indexArray = [];
 
 
 function Odd(name, fileExtension = 'jpg') {
@@ -50,16 +52,21 @@ console.log(oddArray);
 function selectRandomOddNumber() {
   return Math.floor(Math.random() * oddArray.length);
 }
+
 function renderOdd() {
-  let odd1 = selectRandomOddNumber();
-  let odd2 = selectRandomOddNumber();
-  if (odd2 === odd1) {
-    odd2 = selectRandomOddNumber();
+
+  while (indexArray.length < 8) {
+    let randomNum = selectRandomOddNumber();
+    if (!indexArray.includes(randomNum)) {
+      indexArray.push(randomNum);
+    }
   }
-  let odd3 = selectRandomOddNumber();
-  if (odd3 === odd2 || odd3 === odd1) {
-    odd3 = selectRandomOddNumber();
-  }
+
+  let odd1 = indexArray.shift();
+  let odd2 = indexArray.shift();
+  let odd3 = indexArray.shift();
+
+  console.log(indexArray);
 
   image1.src = oddArray[odd1].src;
   image1.alt = oddArray[odd1].name;
@@ -73,6 +80,7 @@ function renderOdd() {
 }
 
 function handleOddClick(event) {
+  counter++;
   console.log(event.target.alt);
   let clickedOdd = event.target.alt;
   for (let i = 0; i < oddArray.length; i++) {
@@ -92,15 +100,44 @@ function handleOddClick(event) {
 function viewResults() {
   let ul = document.querySelector('ul');
   for (let i = 0; i < oddArray.length; i++) {
-    if (oddArray[i].votes > 0) {
-      let li = document.createElement('li');
-      li.textContent = `${oddArray[i].name} had ${oddArray[i].votes}
-    votes and was seen ${oddArray[i].views} times.`;
-      ul.appendChild(li);
-    }
+    let li = document.createElement('li');
+    li.textContent = `${oddArray[i].name} had ${oddArray[i].views} views, and ${oddArray[i].votes} votes.`;
+    ul.appendChild(li);
+
   }
 }
+
 renderOdd();
+
+console.log(oddArray);
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      },
+      {
+        label: '# of Views',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
 
 myContainer.addEventListener('click', handleOddClick);
 viewResultsBtn.addEventListener('click', viewResults);
